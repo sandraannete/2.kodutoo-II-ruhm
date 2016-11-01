05517094c728d67cd157f7998ce1169acee5b5e1
Sandra-Annete Tagam <sandraannete@users.noreply.github.com>
2016-11-01 12:23:14 +0200

    finished

--- test.php
+++
@@ -1,7 +0,0 @@
-<form name="cityselect">
-    <select name="menu" onChange="window.document.location.href=this.options[this.selectedIndex].value;" value="GO">
-        <option selected="selected">Select One</option>
-        <option value="http://www.leeds.com">Leeds</option>
-        <option value="https://www.ekool.eu/index_et.html">Manchester</option>
-    </select>
-</form>
